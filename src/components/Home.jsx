c255b2029a7b74190c084d1a3d9fffb6592f4102
Elezaaby <mahmodalezaby@gmail.com>
2023-02-04 14:26:09 +0200

    add component movies

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import backgroundHome from '../images/home-background.png'
 import ImgCarousel from './ImgCarousel'
+import Movies from './Movies'
 import Viewers from './Viewers'
 
 function Home() {
@@ -9,13 +10,14 @@ function Home() {
     <Container>
       <ImgCarousel />
       <Viewers />
+      <Movies />
     </Container>
   )
 }
 
 const Container = styled.main`
   min-height: calc(100vh - 70px);
-  padding: 0 50px;
+  padding: 0 50px 10px;
   position: relative;
   overflow-x: hidden;
 
